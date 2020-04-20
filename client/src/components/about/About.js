575ae3cceb5911c6f1a0f75627a4dc353810c4d8
cheydolph <cheydolph@ufl.edu>
2020-04-19 21:56:17 -0400

    Minor CSS fixes

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -21,24 +21,23 @@ class About extends React.Component {
           <SideNav />
           <Col>
             <div>
-
               <body style={{}}>
                 <h1 style={{ fontSize: '60px', fontFamily: 'Lekton', 'text-align':'center', 'margin-top': '3vw' }}>
                   About Us</h1>
               </body >
               <br/>
-              <div style={{ marginLeft: '100px', width: 500, height: 'auto', }}>
+              <div align='center'>
                 <img src={profilePic} alt="" />
               </div>
-              <div style={{ marginLeft: '100px', }}>
-                <h1 style={{ fontFamily: 'Lekton', fontSize: '40px' }}>First Name Last Name</h1>
-                <p style={{ fontFamily: 'Lekton' }}> DC Fitness LLC is a private fitness training company. We offer our customers plenty of benefits to give them a leg up on the competition.</p>                <br />
-                <p style={{ fontFamily: 'Lekton' }}> If you are interested in a private consulatation please request an appointment in the Calendar page.</p>
+              <div>
+                <h1 style={{ fontFamily: 'Lekton', fontSize: '40px', 'text-align':'center', 'margin-top': '1vw', 'margin-bottom': '1vw' }}>First Name Last Name</h1>
+                <p style={{ fontFamily: 'Lekton', fontSize: '20px', 'text-align':'center', 'margin-top': '1vw' }}> DC Fitness LLC is a private fitness training company. We offer our customers plenty of benefits to give them a leg up on the competition.</p>                <br />
+                <p style={{ fontFamily: 'Lekton', fontSize: '20px', 'text-align':'center'}}> If you are interested in a private consulatation please request an appointment in the Calendar page.</p>
               </div>
               <br />
-              <div style={{ marginLeft: '100px', }}>
-                <p style={{ fontFamily: 'Lekton' }}> Phone: 123-456-7890</p>
-                <p style={{ fontFamily: 'Lekton' }}> Email: dc_fit@gmail.com</p>
+              <div>
+                <p style={{ fontFamily: 'Lekton', 'text-align':'center' }}> Phone: 123-456-7890</p>
+                <p style={{ fontFamily: 'Lekton', 'text-align':'center' }}> Email: dc_fit@gmail.com</p>
               </div>
             </div>
           </Col>
